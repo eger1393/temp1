0dacb2786d6ea9117b5747036827fde2cba49fad
eger1393 <eger1393@gmail.com>
2022-03-03 11:51:58 +0300

    Перенес документацию из  UserRepository в интерфейс IUserRepository

--- ZELF.Test.Data/Repositories/UserRepository.cs
+++ ZELF.Test.Data/Repositories/UserRepository.cs
@@ -9,10 +9,47 @@ namespace ZELF.Test.Data.Repositories
 {
     public interface IUserRepository
     {
+        
+        /// <summary>
+        /// Добавляет нового пользователя
+        /// </summary>
+        /// <param name="name">Имя пользователя</param>
+        /// <returns>Пользователь</returns>
         public User AddUser(string name);
+        
+        /// <summary>
+        /// Возвращает данные о пользователе, включая его подписчиков
+        /// </summary>
+        /// <param name="id">Ид пользователя</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Когда пользователь с переданным Ид не найден</exception>
         public User GetUser(Guid id);
+        
+        /// <summary>
+        /// Подписывает на пользователя
+        /// </summary>
+        /// <param name="userId">Пользователь которого подписываем</param>
+        /// <param name="toUserId">На кого подписываем</param>
+        /// <returns>Обновленные данные о пользхователе</returns>
+        /// <exception cref="ArgumentException">Если пользователь с переданным Ид не найден</exception>
         public User Subscribe(Guid userId, Guid toUserId);
+        
+        /// <summary>
+        /// Одписать пользователя
+        /// </summary>
+        /// <param name="userId">Пользователь которого отписываем</param>
+        /// <param name="toUserId">От кого отписываем</param>
+        /// <returns>Обновленные данные</returns>
+        /// <exception cref="ArgumentException">Если пользователь с переданным Ид не найден</exception>
+        /// <exception cref="InvalidOperationException">Если пытаемся отписать не подписанного пользователя</exception>
         public User UnSubscribe(Guid userId, Guid toUserId);
+        
+        /// <summary>
+        /// Возвращает пользователей с наибольшим чисслом подписок
+        /// </summary>
+        /// <param name="count">кол-во пользователей которое надо вернуть</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Если переданно не положительное кол-во</exception>
         public IEnumerable<User> GetTop(int count);
     }
     
@@ -25,11 +62,6 @@ namespace ZELF.Test.Data.Repositories
             _context = context;
         }
 
-        /// <summary>
-        /// Добавляет нового пользователя
-        /// </summary>
-        /// <param name="name">Имя пользователя</param>
-        /// <returns>Пользователь</returns>
         public User AddUser(string name)
         {
             var newUser = new User()
@@ -43,12 +75,6 @@ namespace ZELF.Test.Data.Repositories
             return newUser;
         }
 
-        /// <summary>
-        /// Возвращает данные о пользователе, включая его подписчиков
-        /// </summary>
-        /// <param name="id">Ид пользователя</param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException">Когда пользователь с переданным Ид не найден</exception>
         public User GetUser(Guid id)
         {
             var user = _context.Users.Include(x => x.Subscribers).FirstOrDefault(x => x.Id == id);
@@ -57,13 +83,6 @@ namespace ZELF.Test.Data.Repositories
             return user;
         }
 
-        /// <summary>
-        /// Подписывает на пользователя
-        /// </summary>
-        /// <param name="userId">Пользователь которого подписываем</param>
-        /// <param name="toUserId">На кого подписываем</param>
-        /// <returns>Обновленные данные о пользхователе</returns>
-        /// <exception cref="ArgumentException">Если пользователь с переданным Ид не найден</exception>
         public User Subscribe(Guid userId, Guid toUserId)
         {
             var user = GetUser(toUserId);
@@ -74,14 +93,6 @@ namespace ZELF.Test.Data.Repositories
             return user;
         }
 
-        /// <summary>
-        /// Одписать пользователя
-        /// </summary>
-        /// <param name="userId">Пользователь которого отписываем</param>
-        /// <param name="toUserId">От кого отписываем</param>
-        /// <returns>Обновленные данные</returns>
-        /// <exception cref="ArgumentException">Если пользователь с переданным Ид не найден</exception>
-        /// <exception cref="InvalidOperationException">Если пытаемся отписать не подписанного пользователя</exception>
         public User UnSubscribe(Guid userId, Guid toUserId)
         {
             var user = GetUser(toUserId);
@@ -91,13 +102,7 @@ namespace ZELF.Test.Data.Repositories
             _context.SaveChanges();
             return user;
         }
-
-        /// <summary>
-        /// Возвращает пользователей с наибольшим чисслом подписок
-        /// </summary>
-        /// <param name="count">кол-во пользователей которое надо вернуть</param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException">Если переданно не положительное кол-во</exception>
+        
         public IEnumerable<User> GetTop(int count)
         {
             if (count <= 0)
