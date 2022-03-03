fbf4aaeb6759728cfa923fa1813881506c76c6fc
eger1393 <eger1393@gmail.com>
2022-03-03 14:36:34 +0300

    Добавил аннотацию модели для проверки имени пользователя

--- ApiGateway/Models/CreateUser.cs
+++ ApiGateway/Models/CreateUser.cs
@@ -1,7 +1,12 @@
-﻿namespace ApiGateway.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiGateway.Models
 {
     public class CreateUser
     {
+        [Required]
+        [MaxLength(64)]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Name can only contain characters and spaces")]
         public string Name { get; set; }
     }
 }
